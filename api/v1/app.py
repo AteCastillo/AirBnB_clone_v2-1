8bb72634167693ea51c7837ddbf442e76629817f
AteCastillo <atecast@gmail.com>
2021-05-10 11:38:31 -0300

    adding 11, 12 and 13 tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """First time API"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
